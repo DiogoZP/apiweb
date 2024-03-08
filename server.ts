b63dc7c1d058dacc3007c8d770a7a56e65b2393a
Diogo Zago <diogozago04@gmail.com>
2024-03-08 19:59:32 -0300

    Swagger instalado

--- server.ts
+++ server.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import autorRouter from "./routes/autorRouter";
 import livroRouter from "./routes/livroRouter";
+import swaggerUi from "swagger-ui-express";
+import swaggerDoc from "./swaggerDoc.json";
 
 const app = express();
 
 app.use(express.json());
 app.use("/autores", autorRouter);
 app.use("/livros", livroRouter);
+app.use("/docs", swaggerUi.serve);
+
+app.get('/docs', swaggerUi.setup(swaggerDoc));
 
 app.listen(3000, () => {
     console.log("Server rodando na porta 3000");
---
+++ swagger.ts
@@ -0,0 +1,14 @@
+import swaggerAutogen from 'swagger-autogen';
+
+const doc = {
+    info: {
+      title: 'My API',
+      description: 'Description'
+    },
+    host: 'localhost:3000'
+};
+
+const output = './swaggerDoc.json';
+const routes = ['./server.ts'];
+
+swaggerAutogen(output, routes, doc);
